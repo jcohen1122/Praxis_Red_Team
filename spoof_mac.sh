2119171816cde2e3c2e9a9da9920d1705ac6553f
Joshua Cohen <jcohen02@terpmail.umd.edu>
2024-08-08 09:02:21 -0400

    Delete spoof_mac.sh

--- spoof_mac.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-NEW_MAC="$1"
-cd ~/redteam/bluez/tools
-sudo ./bdaddr -i hci0 $NEW_MAC
-sudo systemctl daemon-reload
-sudo systemctl restart bluetooth
-echo "Device reset: new mac active."
